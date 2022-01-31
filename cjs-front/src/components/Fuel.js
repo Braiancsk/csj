f994a7af8135471a984df1df538685daf49cf862
Braian Fellipe <74879806+Braiancsk@users.noreply.github.com>
2022-01-31 13:14:32 -0300

    Delete cjs-front directory

--- cjs-front/src/components/Fuel.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Fuel(props) {
-  return <div style={props.style} className="fuel"></div>;
-}
--- cjs-front/src/components/PrimaryButton.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function PrimaryButton(props) {
-  return <button className="py-[28px] px-[23px] text-center block max-w-[322px] w-full border-0 text-white text-3xl bg-[#2B6688] rounded-[20px]">{props.name}</button>
-}
